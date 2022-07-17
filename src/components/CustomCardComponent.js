09d4fe250930261b46be0e07372c9d78ed6425f9
babazadenihat <nihatbabazade0@gmail.com>
2022-07-17 13:26:37 +0400

    upd 17 july

--- src/components/CustomCardComponent.js
+++ src/components/CustomCardComponent.js
@@ -13,7 +13,8 @@ import add from "../images/icons/add.svg";
 import { CardItemBox, DraggableBox } from "../styledComponents/styled";
 import { Button, makeStyles } from '@material-ui/core';
 import { IntlProvider, FormattedMessage, FormattedNumber } from 'react-intl'
-
+import { text } from '../translations/translation';
+import { useSelector } from 'react-redux';
 
 
 
@@ -35,15 +36,105 @@ export const CustomCardComponent = (props) => {
 
     return (
         <>
-            <Card
+            <ProductCard />
+        </>
+    )
+}
+
+
+const ProductCard = ({ renderRoute }) => {
+    console.log(renderRoute)
+    const { offerCount, currentOffer } = text;
+    const selectedLang = useSelector((state) => state.language.translation)
+    const classes = useStyle();
+
+    return (
+        <div className={classes.cardStyle}>
+            {
+                renderRoute === "home" &&
+                <DraggableBox />
+            }
+            <div>
+                <div className="cardImgCover"><Card.Img variant="top" src={paint1} /></div>
+            </div>
+            <div>
+                <div>
+                    <img src={userPhoto} alt="" className="cardSmallImg" />
+                    <span className={`${classes.cardOwner} ms-2`}>Tahir Salahov</span>
+                </div>
+                <div className={classes.cardTitle}>“No More Voices”</div>
+            </div>
+            <div className='d-flex align-items-center'>
+                <div>{offerCount[selectedLang]}</div>
+                <div>24</div>
+            </div>
+            <div className='d-flex align-items-center'>
+                <div>{currentOffer[selectedLang]}</div>
+                <div>78&#8380;</div>
+            </div>
+            <div className='d-flex align-items-center'>
+                <div>
+                    <img src={heart} alt="" />
+                    <span className='ms-2'>24</span>
+                    {/* burani component etmeli */}
+                </div>
+                <div>
+                    Bitmə tarixi: 2g, 11s
+                </div>
+            </div>
+        </div>
+    )
+}
+
+
+const useStyle = makeStyles(theme => ({
+    cardOwner: {
+        color: "black",
+        fontWeight: "700",
+        fontSize: "12px",
+        lineHeight: "18px",
+        letterSpacing: "-0.5%"
+    },
+    cardTitle: {
+        color: "black",
+        fontWeight: "400",
+        fontStyle: "italic",
+        fontSize: "14px",
+        lineHeight: "16px"
+    },
+    tablist: {
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "center",
+        backgroundColor: "#fff",
+        borderRadius: '100px',
+        padding: "6px"
+    },
+    noStyleBtnLabel: {
+        "& .MuiButton-label": {
+            display: "block"
+        }
+    },
+    cardStyle: {
+        fontFamily: "Poppins",
+        background: "#fff",
+        padding: "17px 15px",
+        boxShadow: "0 4px 6px rgb(0 0 0 / 25%)",
+        borderRadius: "14px",
+        [theme.breakpoints.down("md")]: {
+            width: "100% !important"
+        }
+    }
+}))
+
+
+
+
+
+{/* <Card
                 id={order.toString()}
                 style={style}
                 data-id={id}
-                // draggable={true}
-                // onDragStart={handleDrag}
-                // onDragOver={(e => e.preventDefault())}
-                // onDrop={handleDrop}
-                // id="items"
                 className={classes.cardStyle}
                 key={keyProp}
             >
@@ -81,182 +172,4 @@ export const CustomCardComponent = (props) => {
                         <div className="">Bitmə tarixi: 2g, 11s</div>
                     </ListGroupItem>
                 </ListGroup>
-            </Card>
-        </>
-    )
-}
-
-
-const useStyle = makeStyles(theme => ({
-    tablist: {
-        display: "flex",
-        justifyContent: "space-between",
-        alignItems: "center",
-        backgroundColor: "#fff",
-        borderRadius: '100px',
-        padding: "6px"
-    },
-    noStyleBtnLabel: {
-        "& .MuiButton-label": {
-            display: "block"
-        }
-    },
-    cardStyle: {
-        [theme.breakpoints.down("md")]: {
-            width: "100% !important"
-        }
-    }
-}))
-
-
-
-
-
-
-
-{/* <Card style={{ width: '18rem' }}>
-                                    <Styled.DraggableBox />
-                                    <div className="cardImgCover">
-                                        <Card.Img variant="top" style={{ pointerEvents: "none" }} src={paint2} />
-                                    </div>
-                                    <Card.Body>
-                                        <Card.Title className="d-flex align-items-center">
-                                            <img src={userPhoto} alt="" className="cardSmallImg" />
-                                            <span className="ms-2">Tahir Salahov</span>
-                                        </Card.Title>
-                                        <Card.Text>
-                                            “Le Carnaval (Les Cotillons)”
-                                        </Card.Text>
-                                    </Card.Body>
-
-                                    <ListGroup className="list-group-flush">
-                                        <ListGroupItem className="d-flex justify-content-between">
-                                            <div>Təklif sayı</div>
-                                            <div>24</div>
-                                        </ListGroupItem>
-                                        <ListGroupItem className="d-flex justify-content-between">
-                                            <div>Cari təklif</div>
-                                            <div className="jis-manat">78M</div>
-                                        </ListGroupItem>
-                                    </ListGroup>
-                                    <ListGroup className="list-group-flush">
-                                        <ListGroupItem className="d-flex justify-content-between align-items-baseline">
-                                            <div className="likes">
-                                                <img src={heart} alt="" />
-                                                <span>24</span>
-                                            </div>
-                                            <div className="">Bitmə tarixi: 2g, 11s</div>
-                                        </ListGroupItem>
-                                    </ListGroup>
-                                </Card>
-
-
-
-<Card style={{ width: '18rem', position: 'relative' }}>
-                                    <Styled.DraggableBox />
-                                    <div className="cardImgCover"><Card.Img variant="top" src={paint1} /></div>
-                                    <Card.Body>
-                                        <Card.Title className="d-flex align-items-center">
-                                            <img src={userPhoto} alt="" className="cardSmallImg" />
-                                            <span className="ms-2">Tahir Salahov</span>
-                                        </Card.Title>
-                                        <Card.Text>
-                                            “yes I am here”
-                                        </Card.Text>
-                                    </Card.Body>
-
-                                    <ListGroup className="list-group-flush">
-                                        <ListGroupItem className="d-flex justify-content-between">
-                                            <div>Təklif sayı</div>
-                                            <div>24</div>
-                                        </ListGroupItem>
-                                        <ListGroupItem className="d-flex justify-content-between">
-                                            <div>Cari təklif</div>
-                                            <div className="jis-manat">78M</div>
-                                        </ListGroupItem>
-                                    </ListGroup>
-                                    <ListGroup className="list-group-flush">
-                                        <ListGroupItem className="d-flex justify-content-between align-items-baseline">
-                                            <div className="likes">
-                                                <img src={heart} alt="" />
-                                                <span>24</span>
-                                            </div>
-                                            <div className="">Bitmə tarixi: 2g, 11s</div>
-                                        </ListGroupItem>
-                                    </ListGroup>
-                                </Card>
-
-
-
-<Card style={{ width: '18rem' }}>
-<Styled.DraggableBox />
-<div className="cardImgCover">
-    <Card.Img variant="top" style={{ pointerEvents: "none" }} src={paint2} />
-</div>
-<Card.Body>
-    <Card.Title className="d-flex align-items-center">
-        <img src={userPhoto} alt="" className="cardSmallImg" />
-        <span className="ms-2">Tahir Salahov</span>
-    </Card.Title>
-    <Card.Text>
-        “Le Cossion patadole”
-    </Card.Text>
-</Card.Body>
-
-<ListGroup className="list-group-flush">
-    <ListGroupItem className="d-flex justify-content-between">
-        <div>Təklif sayı</div>
-        <div>24</div>
-    </ListGroupItem>
-    <ListGroupItem className="d-flex justify-content-between">
-        <div>Cari təklif</div>
-        <div className="jis-manat">78M</div>
-    </ListGroupItem>
-</ListGroup>
-<ListGroup className="list-group-flush">
-    <ListGroupItem className="d-flex justify-content-between align-items-baseline">
-        <div className="likes">
-            <img src={heart} alt="" />
-            <span>24</span>
-        </div>
-        <div className="">Bitmə tarixi: 2g, 11s</div>
-    </ListGroupItem>
-</ListGroup>
-</Card>
-
-
-<Card style={{ width: '18rem' }}>
-<Styled.DraggableBox />
-<div className="cardImgCover">
-    <Card.Img variant="top" style={{ pointerEvents: "none" }} src={paint2} />
-</div>
-<Card.Body>
-    <Card.Title className="d-flex align-items-center">
-        <img src={userPhoto} alt="" className="cardSmallImg" />
-        <span className="ms-2">Tahir Salahov</span>
-    </Card.Title>
-    <Card.Text>
-        “Le Cossion fortran
-    </Card.Text>
-</Card.Body>
-
-<ListGroup className="list-group-flush">
-    <ListGroupItem className="d-flex justify-content-between">
-        <div>Təklif sayı</div>
-        <div>24</div>
-    </ListGroupItem>
-    <ListGroupItem className="d-flex justify-content-between">
-        <div>Cari təklif</div>
-        <div className="jis-manat">78M</div>
-    </ListGroupItem>
-</ListGroup>
-<ListGroup className="list-group-flush">
-    <ListGroupItem className="d-flex justify-content-between align-items-baseline">
-        <div className="likes">
-            <img src={heart} alt="" />
-            <span>24</span>
-        </div>
-        <div className="">Bitmə tarixi: 2g, 11s</div>
-    </ListGroupItem>
-</ListGroup>
-</Card> */}
+            </Card> */}
